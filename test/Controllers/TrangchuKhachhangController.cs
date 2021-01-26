2ae74590998076f4bc96540de87347c83949f5a1
linhtruc430@gmail.com <linhtruc430@gmail.com>
2021-01-26 16:57:38 +0700

    hali commit all files

---
+++ test/Controllers/TrangchuKhachhangController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test.Controllers
+{
+    public class TrangchuKhachhangController : Controller
+    {
+        // GET: TrangchuKhachhang
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
